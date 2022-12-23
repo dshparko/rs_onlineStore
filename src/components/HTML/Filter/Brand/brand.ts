739e1a941223d16dac2a1b7f673c231a3ed8ee38
Kiryll Khomchanka <dickdurex265@gmail.com>
2022-12-23 22:11:23 +0300

    fix: detele tag <form>

--- src/components/HTML/Filter/Brand/brand.ts
+++ src/components/HTML/Filter/Brand/brand.ts
@@ -5,52 +5,52 @@ export const brand = `
       <p class='brandTitle'>Brand</p>
       <div class='brandCheckbox'>
     <div class='brandItem'>
-    <form>
+    
   <input type="checkbox">
   <label>xxx</label>
-</form>    
+   
 <p class='productAvailability'>(x/x)</p>
     </div>
     <div class='brandItem'>
-    <form>
+    
   <input type="checkbox">
   <label>xxx</label>
-</form>    
+    
 <p class='productAvailability'>(x/x)</p>
     </div>
     <div class='brandItem'>
-    <form>
+    
   <input type="checkbox">
   <label>xxx</label>
-</form>    
+   
 <p class='productAvailability'>(x/x)</p>
     </div>
     <div class='brandItem'>
-    <form>
+    
   <input type="checkbox">
   <label>xxx</label>
-</form>    
+   
 <p class='productAvailability'>(x/x)</p>
     </div>
     <div class='brandItem'>
-    <form>
+   
   <input type="checkbox">
   <label>xxx</label>
-</form>    
+    
 <p class='productAvailability'>(x/x)</p>
     </div>
     <div class='brandItem'>
-    <form>
+    
   <input type="checkbox">
   <label>xxx</label>
-</form>    
+   
 <p class='productAvailability'>(x/x)</p>
     </div>
     <div class='brandItem'>
-    <form>
+    
   <input type="checkbox">
   <label>xxx</label>
-</form>    
+
 <p class='productAvailability'>(x/x)</p>
     </div>
       </div>
--- src/components/HTML/Filter/Category/category.ts
+++ src/components/HTML/Filter/Category/category.ts
@@ -4,48 +4,48 @@ export const category = `<div class='categoryWrapper'>
 <p class='categoryTitle'>Category</p>
 <div class='categoryCheckbox'>
 <div class='categoryItem'>
-<form>
+
   <input type="checkbox">
   <label>Jackets</label>
-</form>
+
 <p class='productAvailability'>(x/x)</p>
 </div>
 <div class='categoryItem'>
-<form>
+
   <input type="checkbox">
   <label>xxx</label>
-</form>
+
 <p class='productAvailability'>(x/x)</p>
 </div>
 <div class='categoryItem'>
-<form>
+
   <input type="checkbox">
   <label>xxx</label>
-</form><p class='productAvailability'>(x/x)</p>
+<p class='productAvailability'>(x/x)</p>
 </div>
 <div class='categoryItem'>
-<form>
+
   <input type="checkbox">
   <label>xxx</label>
-</form><p class='productAvailability'>(x/x)</p>
+<p class='productAvailability'>(x/x)</p>
 </div>
 <div class='categoryItem'>
-<form>
+
   <input type="checkbox">
   <label>xxx</label>
-</form><p class='productAvailability'>(x/x)</p>
+<p class='productAvailability'>(x/x)</p>
 </div>
 <div class='categoryItem'>
-<form>
+
   <input type="checkbox">
   <label>xxx</label>
-</form><p class='productAvailability'>(x/x)</p>
+<p class='productAvailability'>(x/x)</p>
 </div>
 <div class='categoryItem'>
-<form>
+
   <input type="checkbox">
   <label>xxx</label>
-</form><p class='productAvailability'>(x/x)</p>
+<p class='productAvailability'>(x/x)</p>
 </div>
 </div>
 </div>`
